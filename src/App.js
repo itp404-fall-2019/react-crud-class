a275e33a6e78f91e95c44ff2c8a5154267f1e5ae
David Tang <dtang859@gmail.com>
2019-11-05 19:14:07 -0800

    Sets up posts page and create a post page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
 import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  NavLink
+} from "react-router-dom";
 import "./App.css";
 
 const API = "https://dtang-in-memory-api.herokuapp.com";
 
 export default class App extends React.Component {
+  render() {
+    return (
+      <Router>
+        <h1>My Blog</h1>
+        <nav>
+          <ul>
+            <li>
+              <NavLink to="/" exact={true}>
+                Posts
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/posts/write">Write a Post</NavLink>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/" exact={true} component={PostsPage} />
+          <Route path="/posts/write" component={PostForm} />
+        </Switch>
+      </Router>
+    );
+  }
+}
+
+class PostsPage extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -18,16 +49,29 @@ export default class App extends React.Component {
   }
   render() {
     return (
-      <Router>
-        <h1>My Blog</h1>
-        <nav>
-          <ul>
-            {this.state.posts.map(post => {
-              return <li key={post.id}>{post.title}</li>;
-            })}
-          </ul>
-        </nav>
-      </Router>
+      <ul>
+        {this.state.posts.map(post => {
+          return <li key={post.id}>{post.title}</li>;
+        })}
+      </ul>
+    );
+  }
+}
+
+class PostForm extends React.Component {
+  render() {
+    return (
+      <form>
+        <div>
+          <label htmlFor="title">Title</label>
+          <input type="text" id="title" />
+        </div>
+        <div>
+          <label htmlFor="body">Body</label>
+          <textarea id="body"></textarea>
+        </div>
+        <button>Publish</button>
+      </form>
     );
   }
 }
